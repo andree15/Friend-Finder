6177b1fba505792bd8813b3cce1b5ddf96ac5647
Andy Edstrom <andyedstrom115@gmail.com>
2020-07-01 21:32:55 -0400

    html and functionality edits

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsData = [
+  {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Mr 5S",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        5,5,5,5,5,5,5,5,5,
+      ]
+  },
+  {
+    "name":"ms 1s",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,1,1,1,1,1,1,1,1,
+        1
+      ]
+  },
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsData;
+  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,76 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
 module.exports = function (app) {
-    // HTML GET Requests
-    // Below code handles when users “visit” a page.
-    // In each of the below cases the user is shown an HTML page of content
+
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
     app.get("/api/friends", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/tables.html"));
+        res.json(friendsData);
     });
+
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+
     app.post("/api/friends", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        console.log(req.body)
+        var newPerson = req.body;
+
+        let winner;
+        let winnerDiff= Infinity;
+
+        for (let i = 0; i < friendsData.length; i++) {
+            const element = friendsData[i];
+            const elementTotalScore = 8;
+            let difference=0;
+            //loop through the element scores, comparing with the newPerson scores
+            for (let j = 0; j < element.scores.length; j++) {
+                const npScore= newPerson.scores[j]
+                const thisGuyScore= element.scores[j]
+                let unmatchedness = npScore - thisGuyScore
+                unmatchedness = Math.abs(unmatchedness)
+                difference += unmatchedness
+            }
+            if (difference < winnerDiff){
+                winner=element
+                winnerDiff=difference
+            }
+            console.log(difference)
+            console.log(element);
+            }
+        friendsData.push(newPerson)
+        res.json(winner);
+    });
+
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+    app.post("/api/clear", function (req, res) {
         // Empty out the arrays of data
-        // tableData.length = 0;
-        // waitListData.length = 0;
+        
+        tableData.length = 0;
+        waitListData.length = 0;
+
         res.json({ ok: true });
     });
 };
